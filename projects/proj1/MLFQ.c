04ed81e762dcf065f3663098cf38f7200e53944c
breakeuropa <johansc@siue.edu>
2025-10-16 12:11:59 -0500

    Add files via upload

--- projects/proj1/MLFQ.c
+++ projects/proj1/MLFQ.c
@@ -6,24 +6,36 @@
 #include <sys/time.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 
 #define STACK_SIZE SIGSTKSZ
-#define MAX_PROCESSES 5
-#define MAX_QUEUE_LENGTH 3
-#define PUSER 50
+
+int PUSER = 50;
+int QUANTA = 0;
 
 typedef unsigned int tid_t;
 typedef struct thread
 {
 	tid_t tid;
       	ucontext_t ctx;
+	int prio;
+	int pnice;
+	int estCPU;
       	struct thread *next;
 } thread_t;
-
+/*
+typedef struct queue
+{
+	struct queue *next;
+	thread_t* tHead;
+} queue;
+*/
 
 tid_t tid_counter = 0;
 thread_t *scheduler_thread = NULL;
 thread_t *threads = NULL;
+
+thread_t * queue[128] = {0};
 
 void set_timer();
 
@@ -38,8 +50,20 @@ void join()
 //RR
 void scheduler()
 {
-      	if(threads == NULL)
+  	if((QUANTA % 100) == 0)
 	{
+		thread_t *temp = threads;
+
+		while(temp!=NULL)
+		{
+			temp->estCPU = (2/3) * temp->estCPU + temp->prio;
+			printf("temp's new prio: %d\n", temp->prio);
+			temp = temp->next;
+		}
+	}
+	if(threads == NULL)
+	{
+		printf("shit i sempty\n");
 	    	return;
       	}
 	else
@@ -53,6 +77,37 @@ void scheduler()
 	    	}
 	    	printf("\n");
       	} //view of available threads when scheduler runs
+
+	thread_t *current = threads;
+
+	if ((current->estCPU % 4) == 0)
+	{
+		printf("\tchanging priority...\n");
+		current->prio = PUSER + (current->estCPU / 4) + 2 * current->pnice;
+		
+		if (queue[current->prio] == 0)
+		{
+			queue[current->prio] = current;
+		}
+		else
+		{
+			thread_t *prev = NULL;
+			thread_t *t = queue[current->prio];
+			while (t->next != NULL)
+			{
+				printf("t = %d, ", t->tid);
+				t = t->next;
+			}
+			t->next = current;
+			printf("\nattemtpgint to print queue...\n\n");
+			printf("queue[%d] has: ", queue[current->prio]);
+		}
+		
+		printf("\tcurrent's prio is: %d\n", current->prio);
+	}
+
+	
+	printf("b4 thread reset bs idk\n");
 
       	thread_t *oldhead = threads;
       	threads = threads->next;
@@ -72,8 +127,10 @@ void scheduler()
 	    	}
 	    	temp->next = oldhead;
 	}
-
+	
+	printf("b4 timer\n");
       	set_timer();
+	current->estCPU += 1;
       	swapcontext(&(scheduler_thread->ctx), &(threads->ctx)); //takes current context, records state, loads context swapping to
 								//from, to
 }
@@ -100,6 +157,9 @@ void set_timer()
 	    	perror("Setting timer");
 	    	exit(EXIT_FAILURE);
   	}
+
+	printf("\nTotal Quanta: %d\n\n", QUANTA);
+	QUANTA += 1;
 }
 
 
@@ -124,8 +184,17 @@ void init_scheduler()
       	set_timer(); 
 }
 
+
+void insertAtHead(thread_t **head, thread_t *thread)
+{
+	thread->next = *head;
+	*head = thread;
+	printf("inserted @ mind blowing head!!!\n");
+}
+
 void thread_create(void (startfunc)())
 {
+	printf("\n\ncreating thread...\n\n");
 	thread_t *newthread = (thread_t*)malloc(sizeof(thread_t));
 
 	if(scheduler_thread == NULL)	
@@ -141,6 +210,15 @@ void thread_create(void (startfunc)())
       	newthread->ctx.uc_stack.ss_flags = 0;			//idk?
       	newthread->tid = tid_counter;				//send tid counter (global)
       	makecontext(&newthread->ctx, startfunc, 1, newthread->tid);	//associate startfunc (passed in) w/ context created
+	
+	newthread->pnice = rand()%21;
+	newthread->estCPU = 0;
+	newthread->prio = PUSER + newthread->estCPU;
+
+	printf("thread%d's penice is %d & has prio of %d\n", newthread->tid, newthread->pnice, newthread->prio);
+
+	insertAtHead(&queue[newthread->prio], newthread);
+	printf("queue[%d] has thread %d\n", queue[newthread->prio], newthread->tid);
 
       	newthread->next = threads;				//head insert basically
       	threads = newthread;
@@ -164,7 +242,10 @@ int main()
 	thread_create(test);
       	thread_create(test);
       	thread_create(test);
-	
+      	thread_create(test);
+      	thread_create(test);
+      	thread_create(test);
+      	thread_create(test);
       	join();			//makes main wait
 
       	return 0;

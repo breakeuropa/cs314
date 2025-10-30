3a3eabe9f7d04cee78ae6dd2166d13aee4abf209
breakeuropa <johansc@siue.edu>
2025-10-30 11:10:19 -0500

    Add files via upload

--- 10week/directoryDescend.c
+++ 10week/directoryDescend.c
@@ -3,37 +3,88 @@
 #include <unistd.h>
 #include <dirent.h>
 #include <sys/stat.h>
+#include <string.h>
 
-int main(int argc, char *argv[])
+#define MAXBINS 10000
+
+void directoryDescend(const char *dirname, int binWidth, int *bin)
 {
+	
 	struct dirent *pDirent;
 	DIR *pDir;
 	struct stat fStat;
 
+	char path[4096];
+	
+	//printf("opening thing\n");
 
-	if (argc != 2) //wrong arguenmtns
+	pDir = opendir(dirname); //open what i tell it to
+
+	//printf("arg1 is %s\n", argv[1]);
+
+	if (pDir == NULL)
+	{
+		printf("is null\n");
+		return;
+	}
+	
+	while ((pDirent = readdir(pDir)) != NULL)
+	{
+		if (strcmp(pDirent->d_name, ".") == 0 || strcmp(pDirent->d_name, "..") == 0)
+		{
+			continue;
+		}	
+		snprintf(path, sizeof(path), "%s/%s", dirname, pDirent->d_name);
+
+		if (stat(path, &fStat) == -1)
+		{
+			printf("stat failed\n");
+			return;
+		}
+		
+		printf("size: %ld\tname: [%s]\n", fStat.st_size, pDirent->d_name);
+		
+		if (S_ISREG(fStat.st_mode))
+		{
+			int binIndex = fStat.st_size / binWidth;
+			if (binIndex < MAXBINS)
+			{
+				bin[binIndex]++;
+			}
+		}
+		else if (S_ISDIR(fStat.st_mode))
+		{
+			printf("\nDirectory Contents:\n");	
+			directoryDescend(path, binWidth, bin);
+		}
+	
+		//printf("size: %ld\tname: [%s]\n", fStat.st_size, pDirent->d_name);
+	}
+
+	closedir(pDir);
+}
+
+int main(int argc, char *argv[])
+{
+	int bin[MAXBINS] = {0};
+	int binWidth = atoi(argv[2]);
+
+	if (argc != 3) //wrong arguenmtns
 	{
 		printf("no\n");
 		return 1;
 	}
 	
-	printf("openibg thing\n");
-	pDir = opendir(argv[1]); //open what i tell it to
-	printf("arg1 is %s\n", argv[1]);
+	directoryDescend(argv[1], binWidth, bin);
 
-	if (pDir == NULL)
+	printf("\nHistogram:\n");
+	for (int i = 0; i < MAXBINS; i++)
 	{
-		printf("bithc is null\n");
-		return 1;
+		if (bin[i] > 0)
+		{
+			printf("\tbin: %d-%d %d files\n", (i * binWidth), ((i + 1) * binWidth - 1), bin[i]);
+		}
 	}
-	
-	printf("while\n");	
-	while ((pDirent = readdir(pDir)) != NULL)
-	{
-		printf("[%s]\t\t\t\tsize: %ld\n", pDirent->d_name, fStat.st_size);
-	}
-
-	closedir(pDir);
 
 	return 0;
 }

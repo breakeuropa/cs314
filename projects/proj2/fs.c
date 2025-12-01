5ac99202c909073617062008d6e7cc420c19ce27
breakeuropa <johansc@siue.edu>
2025-11-30 20:47:34 -0600

    Add files via upload

--- projects/proj2/fs.c
+++ projects/proj2/fs.c
@@ -1,6 +1,31 @@
 #include "fs.h"
 
-unsigned char* fs;
+#define NUMBLOCKS (FSSIZE/BLOCKSIZE)
+#define NUMFAT NUMBLOCKS
+#define ROOT 0
+unsigned char* fs; 
+
+typedef struct header
+{
+	char name[256];
+	int fsSize;
+	int fatStart;
+	int fatSize;
+	int rootStart;
+}header;
+
+typedef struct block
+{
+	char data[512];
+}block;
+
+typedef struct dirent
+{
+	char name[256];
+	int type;
+	int size;
+	int firstblock;	
+}dirent;
 
 void mapfs(int fd)
 {
@@ -9,8 +34,10 @@ void mapfs(int fd)
       perror("mmap failed");
       exit(EXIT_FAILURE);
   }
+  fprintf(stdout, "mapped fs\n");
 }
 
+unsigned short *FAT;
 
 void unmapfs()
 {
@@ -20,24 +47,149 @@ void unmapfs()
 
 void formatfs()
 {
+	memset(fs, 0, FSSIZE);
+	struct header *h = (struct header *)fs;
+	//h->name[255] = '\0';
+	h->fsSize = FSSIZE;
+	h->fatStart = BLOCKSIZE;
+	h->fatSize = NUMFAT * sizeof(unsigned short);
+	h->rootStart = h->fatStart + h->fatSize;
 
+	FAT = (unsigned short *)(fs + h->fatStart);
+
+	for (int i = 0; i < NUMFAT; i++)
+	{
+		FAT[i] = 0;
+	}
+
+	FAT[0] = -1;
+
+	dirent *root = (dirent *)(fs + h->rootStart);
+
+	int maxDirents = BLOCKSIZE / sizeof(dirent);
+
+	for (int i = 0; i < maxDirents; i++)
+	{
+		root[i].name[0] = '\0';
+		root[i].type = 0;
+		root[i].size = 0;
+		root[i].firstblock = 0;
+	}
+
+	fprintf(stdout, "Formatted the new filesystem! FAT[0] points to %d and Header says fat starts at %d!!!\n", FAT[0], h->fatStart);
 }
 
 
 void loadfs()
 {
+	struct header *h = (struct header*)fs;
 
+	FAT = (unsigned short*)(fs + h->fatStart);
+
+	fprintf(stdout, "Loaded filesystem with FAT starting at %p\n", h->fatStart);
 }
 
 
 void lsfs()
 {
-  
+	header *h = (header *)fs;
+	dirent *root = (dirent *)(fs + h->rootStart);
+	int maxDirents = BLOCKSIZE / sizeof(dirent);
+	fprintf(stdout, "Listing contents...\n");
+
+	for (int i = 0; i < maxDirents; i++)
+	{
+		if (root[i].name[0] != '\0')
+		{
+			fprintf(stdout, "%s \tsize: %d firstblock: %d\n", root[i].name, root[i].size, root[i].firstblock);
+		}
+	}
+}
+
+int fatBlockFinder()
+{
+	for (int i = 0; i < NUMFAT; i++)
+	{
+		if (FAT[i] == 0)
+		{
+			return i;
+		}
+	}
+	return -1;
+}
+
+int buildChain(int blocksNeeded)
+{
+	int first = -2;
+	int prev = -2;
+
+	for (int i = 0; i < blocksNeeded; i++)
+	{
+		int new = fatBlockFinder();
+		FAT[new] = USHRT_MAX;
+
+		if (prev != -2)
+		{
+			FAT[prev] = new;
+		}
+		if (i == 0)
+		{
+			first = new;
+		}
+
+		prev = new;
+	}
+
+	return first;
 }
 
 void addfilefs(char* fname)
 {
+	FILE* fileToAdd = fopen(fname, "r");
+	struct stat fstat;
+	if (stat(fname, &fstat) != 0)
+	{
+		perror("stat error");
+		exit(1);
+	}
 
+	int fileSize = fstat.st_size;
+	int blocksNeeded = (fileSize + BLOCKSIZE - 1) / BLOCKSIZE;
+	int chain = buildChain(blocksNeeded);
+
+	int current = chain; //index of first data block
+	int remain = fileSize;	//# of bytes left
+
+	while (current != USHRT_MAX && remain > 0)
+	{
+		unsigned char *blockPtr = fs + sizeof(header) + NUMFAT * sizeof(unsigned short) + current * BLOCKSIZE;
+		int read = (remain < BLOCKSIZE) ? remain : BLOCKSIZE;
+
+		fread(blockPtr, 1, read, fileToAdd);
+		remain -= read;
+		current = FAT[current];
+	}
+
+	fclose(fileToAdd);
+
+	header *h = (header *)fs;
+	dirent *root = (dirent *)(fs + h->rootStart);
+
+	int maxDirents = BLOCKSIZE / sizeof(dirent);
+	for (int i = 0; i < maxDirents; i++)
+	{
+		if (root[i].name[0] == '\0')
+		{
+			strcpy(root[i].name, fname);
+			root[i].type = 1;
+			root[i].size = fileSize;
+			root[i].firstblock = chain;
+			break;
+		}
+	}
+
+
+	fprintf(stdout, "Added %s which used %d blocks! first block in chain: %d\n", fname, blocksNeeded, chain);
 }
 
 
--- projects/proj2/fs.h
+++ projects/proj2/fs.h
@@ -4,8 +4,17 @@
 #include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/mman.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <getopt.h>
+#include <libgen.h>
+#include <sys/stat.h>
+#include <limits.h>
 
 #define FSSIZE 10000000
+#define BLOCKSIZE 512
 
 extern unsigned char* fs;
 
